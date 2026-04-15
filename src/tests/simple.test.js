7ea5270ab3317bae0260c85a7e0947206a230853
Getuar Jakupi <getuar.j1@gmail.com>
2026-04-15 16:00:49 +0200

    test: WEBHOOK trigger

--- src/tests/simple.test.js
+++ src/tests/simple.test.js
@@ -4,3 +4,9 @@ import assert from "node:assert/strict";
 test("basic test", () => {
   assert.equal(2 + 2, 4, "2 + 2 should equal 4");
 });
+
+test("object equality test", () => {
+  const obj1 = { a: 1, b: 2 };
+  const obj2 = { a: 1, b: 2 };
+  assert.deepEqual(obj1, obj2, "Objects should be deeply equal");
+});
