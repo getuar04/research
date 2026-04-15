43b05358b982f1de2c3bd5846705b246ef79ee35
Getuar Jakupi <getuar.j1@gmail.com>
2026-04-15 15:02:17 +0200

    test: correct tests

--- src/tests/simple.test.js
+++ src/tests/simple.test.js
@@ -2,5 +2,5 @@ import test from "node:test";
 import assert from "node:assert/strict";
 
 test("basic test", () => {
-  assert.equal(2 + 2, 5, "2 + 2 should equal 4");
+  assert.equal(2 + 2, 4, "2 + 2 should equal 4");
 });
