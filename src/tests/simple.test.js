796c55b2b8e425254f57e1f18e26fe6eb9e6dc3c
Getuar Jakupi <getuar.j1@gmail.com>
2026-04-17 09:31:02 +0200

    test2: trigger webhook

--- src/tests/simple.test.js
+++ src/tests/simple.test.js
@@ -10,9 +10,3 @@ test("object equality test", () => {
   const obj2 = { a: 1, b: 2 };
   assert.deepEqual(obj1, obj2, "Objects should be deeply equal");
 });
-
-test("array equality test", () => {
-  const arr1 = [1, 2, 3];
-  const arr2 = [1, 2, 3];
-  assert.deepEqual(arr1, arr2, "Arrays should be deeply equal");
-});
